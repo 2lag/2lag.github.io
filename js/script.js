01f7eac27363ca349e6e200d98b3aac76995cdea
aiden <96544487+2lag@users.noreply.github.com>
2022-08-20 23:13:25 -0500

    how did i not notice the spelling error earlier :|
    looking back it bothered me so much oml

--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ const commands = {
 	help: 
 	{
 		cmd: 'help',
-		res: 'Available commands:<br><br># <span class="red">aboutme</span> - Some information about me.<br># <span class="red">clear</span> - Really, it just clear. <br># <span class="red">screenfetch</span> - Fetches system information in the terminal.<br># <span class="red">links</span> - Links for others sites.<br># <span class="red">visitors</span> - How many times people visited this site.<br># <span class="red">music</span> - Usage: music [play/stop/link]'
+		res: 'Available commands:<br><br># <span class="red">aboutme</span> - Some information about me.<br># <span class="red">clear</span> - Really, it just clear. <br># <span class="red">screenfetch</span> - Fetches system information in the terminal.<br># <span class="red">links</span> - Other socials.<br># <span class="red">visitors</span> - How many times people visited this site.<br># <span class="red">music</span> - Usage: music [play/stop/link]'
 	}
 }
 
