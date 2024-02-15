7e1c8f9e6b599e2ac3d67dbdfc2f6e9f8ed9f178
aiden <96544487+2lag@users.noreply.github.com>
2024-02-15 10:51:08 -0600

    why am i dumb

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-let lastUpdate = "02.13.24 (MM.DD.YYYY)"
-let commits = "342"
+let lastUpdate = "02.15.24 (MM.DD.YYYY)"
+let commits = "345"
 let visits = 1898; // count before it broke :( now we rng it lmfao
 var enableKeys = false;
 $( function() {
